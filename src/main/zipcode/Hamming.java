5e62e649da7d1338d0477b2cc2c75c970502daac
Nathan <natefintak@gmail.com>
2025-04-02 17:19:14 -0400

    in progress

--- src/main/zipcode/Hamming.java
+++ src/main/zipcode/Hamming.java
@@ -1,10 +1,29 @@
 package zipcode;
 
 public class Hamming {
+    String str1 = "";
+    String str2 = "";
+
     public Hamming(String s, String s1) {
+        this.str1 = s;
+        this.str2 = s1;
     }
 
     public int getHammingDistance() {
-        return -1;
+        int counter = 0;
+        while (true) {
+            try {
+            if (this.str1.length() == this.str2.length()) {
+                for (int i = 0; i < this.str1.length(); i++) {
+                    if (this.str1.charAt(i) != this.str2.charAt(i)) {
+                        counter++
+                    }
+                }
+            } catch (IllegalArgumentException e) {
+
+                }
+            }
+        }
+        return counter;
     }
 }
